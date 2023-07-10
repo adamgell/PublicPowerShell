8a90aeb66d0de3a61fa2666ab8d39cc51621c84a
Adam <27519+adamgell@users.noreply.github.com>
2023-07-10 16:48:50 -0400

    fixed detect and remediate apps

--- RemoveApps/DetectApps.ps1
+++ RemoveApps/DetectApps.ps1
@@ -13,23 +13,27 @@ Run as: User/Admin
 Context: 32 & 64 Bit
 #> 
 try {
-    Start-Transcript -Path "$env:TEMP\$($(Split-Path $PSCommandPath -Leaf).ToLower().Replace(".ps1",".log"))" | Out-Null
-    $pathstoremove = "C:\Program Files\WindowsApps\Microsoft.MSPaint_1.0.46.0_x64__8wekyb3d8bbwe","C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.12.10983.0_x64__8wekyb3d8bbwe"
+    Start-Transcript -Path "$env:TEMP\$($(Split-Path $PSCommandPath -Leaf).ToLower().Replace(".ps1",".log"))" -Append
+    $pathstoremove = "C:\Program Files\WindowsApps\Microsoft.MSPaint_1.0.46.0_x64__8wekyb3d8bbwe", "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.12.10983.0_x64__8wekyb3d8bbwe"
     $iftrue = $false
     foreach ($path in $pathstoremove) {
-        Write-Host "Testing to see if $path exists"
+        if($iftrue -eq $true) {
+            break; # break out of the loop if the path is found. this will run the remediation script.
+        }
+        Write-Host "Testing to see if $path exists ..."        
         $iftrue = Test-Path $path
-        $iftrue = $true 
     }
-    
-    if($iftrue){
-        return 1
-    }else{
-        return 0
+    if ($iftrue) {
+        Write-Output "Not Compliant"
+        Exit 1
     }
+    else {
+        Write-Output "Compliant"
+        Exit 0
+    } 
     Stop-Transcript | Out-Null
 }
-catch{ 
-    Write-Output $_.ErrorDetails
-    exit 0
+catch {
+    Write-Host "An error occurred:"
+    Write-Host $_
 }
--- RemoveApps/RemediateApps.ps1
+++ RemoveApps/RemediateApps.ps1
@@ -13,17 +13,26 @@ Run as: User/Admin
 Context: 32 & 64 Bit
 #> 
 
-try {
-    Start-Transcript -Path "$env:TEMP\$($(Split-Path $PSCommandPath -Leaf).ToLower().Replace(".ps1",".log"))" | Out-Null
-    foreach ($path in $pathstoremove) {
-        $pathstoremove = "C:\Program Files\WindowsApps\Microsoft.MSPaint_1.0.46.0_x64__8wekyb3d8bbwe","C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.12.10983.0_x64__8wekyb3d8bbwe"
-        Write-Host "Removing $path"
-        Remove-AppxProvisionedPackage -Online -PackageName $path -AllUsers | Out-Null
-        Remove-AppxProvisionedPackage -Online -Package $path | Out-Null
-    }
-    Stop-Transcript | Out-Null
-}   
-catch {
-    Write-Output $_.ErrorDetails
-    exit 0
+<#
+Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\PackageRepository\Packages\Microsoft.Microsoft3DViewer_2.1803.8022.0_neutral_~_8wekyb3d8bbwe
+Get-AppxPackage -allusers *Microsoft.WebpImageExtension * | Remove-AppxPackage –AllUsers
+Get-AppxProvisionedPackage –online | where-object {$_.packagename –like "PackageName"} | Remove-AppxProvisionedPackage –online
+#>
+$ErrorActionPreference = 'Continue'
+
+Start-Transcript -Path "$env:TEMP\$($(Split-Path $PSCommandPath -Leaf).ToLower().Replace(".ps1",".log"))" -Append
+$pathstoremove = "C:\Program Files\WindowsApps\Microsoft.MSPaint_1.0.46.0_x64__8wekyb3d8bbwe", "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.12.10983.0_x64__8wekyb3d8bbwe"
+foreach ($path in $pathstoremove) {
+    $app = Split-Path -Path $path -Leaf
+    Write-Host "Querying for Application ... $app"
+    Remove-AppxProvisionedPackage -Online -PackageName $path -AllUsers
+    Remove-AppxProvisionedPackage -Online -Package $path 
+    Get-AppxPackage -AllUsers | Where-Object { $_.PackageFullName -eq "$app" } | Remove-AppxPackage -AllUsers
+    Get-AppxPackage -AllUsers | Where-Object { $_.PackageFullName -eq "$app" } | Remove-AppxPackage 
 }
+Stop-Transcript | Out-Null
+
+
+
+
+
