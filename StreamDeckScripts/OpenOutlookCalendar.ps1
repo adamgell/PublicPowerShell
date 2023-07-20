19574178ecc2fdb102ce434ea867f8bbcc0f921b
Adam <27519+adamgell@users.noreply.github.com>
2023-07-19 20:37:46 -0400

    Create OpenOutlookCalendar.ps1
    use a powershell launch plugin or call powershell.exe with the direct path to the script.

---
+++ StreamDeckScripts/OpenOutlookCalendar.ps1
@@ -0,0 +1,29 @@
+Param(
+    [string] $proc="OUTLOOK",
+    [string] $adm
+)
+Clear-Host
+
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class WinAp {
+      [DllImport("user32.dll")]
+      [return: MarshalAs(UnmanagedType.Bool)]
+      public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+      [DllImport("user32.dll")]
+      [return: MarshalAs(UnmanagedType.Bool)]
+      public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+"@
+$p = Get-Process | Where-Object {$_.mainWindowTitle} | Where-Object {$_.Name -like "$proc"}
+if (($null -eq $p) -and ($adm -ne "")) {
+    Start-Process "$proc" -Verb runAs -ArgumentList "/select outlook:calendar"
+} elseif (($null -eq $p) -and ($adm -eq "")) {
+    Start-Process "$proc" -ArgumentList "/select outlook:calendar"
+} else {
+    $h = $p.MainWindowHandle
+    [void] [WinAp]::SetForegroundWindow($h)
+    [void] [WinAp]::ShowWindow($h, 3)
+}
