18f669ef15d58db8f1599f5a6b087360b534cd92
Adam <27519+adamgell@users.noreply.github.com>
2024-03-27 13:31:45 -0400

    Update Enable-UsedSpace-Bitlocker-TPM.ps1

--- Bitlocker/Enable-UsedSpace-Bitlocker-TPM.ps1
+++ Bitlocker/Enable-UsedSpace-Bitlocker-TPM.ps1
@@ -1,17 +1,16 @@
-
-$BitLockerOSVolume = Get-BitLockerVolume -MountPoint $env:SystemRoot
+$BitLockerOSVolume = Get-BitLockerVolume -MountPoint $env:SystemDrive
+$UsedSpace = manage-bde -status $ENV:SystemDrive | Select-String "Used Space Only Encrypted"
 
 function Start-Encryption {
     Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -TpmProtector
     Enable-BitLocker -MountPoint $env:SystemDrive -RecoveryPasswordProtector -SkipHardwareTest
     Start-Sleep -Seconds 15
-    $KEY = Get-BitLockerVolume -MountPoint $env:SystemRoot
-    Backup-BitLockerKeyProtector -MountPoint $env:SystemRoot -KeyProtectorId $KEY.KeyProtector[1].KeyProtectorId
+    Backup-BitLockerKeyProtector -MountPoint $env:SystemDrive -KeyProtectorId $BitLockerOSVolume.KeyProtector[1].KeyProtectorId
 }
 
-if (($BitLockerOSVolume.VolumeStatus -like "FullyEncrypted") -and ($BitLockerOSVolume.ProtectionStatus -eq "on")) {
+if (($BitLockerOSVolume.VolumeStatus -like "FullyEncrypted") -and ($null -ne $UsedSpace)) {
 	Write-Host "Drive already encrypted under used space only. Let's remove this encryption and re-encrypt with full disk encryption."
-    Disable-BitLocker -MountPoint $env:SystemRoot
+    Disable-BitLocker -MountPoint $env:SystemDrive
     Start-Sleep -Seconds 120
     Start-Encryption
 } else {
