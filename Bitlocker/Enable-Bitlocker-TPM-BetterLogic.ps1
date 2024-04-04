7f064bf651855ac06c057d3f84207fafe2b6b581
Adam <27519+adamgell@users.noreply.github.com>
2024-04-04 11:07:45 -0400

    sent it

--- Bitlocker/Enable-Bitlocker-TPM-BetterLogic.ps1
+++ Bitlocker/Enable-Bitlocker-TPM-BetterLogic.ps1
@@ -16,12 +16,14 @@ function Write-Log {
         [ValidateNotNullOrEmpty()]
         [string]$Message
     )
- 
+
+    $computername = $env:COMPUTERNAME
+
     [pscustomobject]@{
         Time     = (Get-Date -Format "yyyy-MM-dd HH:mm")
         Severity = $Severity
         Message  = $Message
-    } | Export-Csv -Path "$env:Temp\LogFile.csv" -Append -NoTypeInformation
+    } | Export-Csv -Path "C:\Bitlocker\LogFile_$computername.csv" -Append -NoTypeInformation
 }
 
 #
@@ -34,14 +36,16 @@ $TPMPresent = [bool](Get-WmiObject -class WIN32_TPM -Namespace root\CIMv2\Securi
 $TPMReady = [bool](Get-WmiObject -class WIN32_TPM -Namespace root\CIMv2\Security\Microsofttpm).IsActivated_InitialValue
 $BitLockerReadyDrive = (Get-BitLockerVolume -MountPoint $env:SystemDrive -ErrorAction SilentlyContinue).MountPoint 
 $BitLockerDecrypted = (Get-BitLockerVolume -MountPoint $env:SystemDrive -ErrorAction SilentlyContinue).VolumeStatus
-$KeyProtectorLocation = "C:\Users\Administrator\Desktop\KeyProtector.txt"
 
 $managebde = manage-bde -status $ENV:SystemDrive
 $UsedSpace = $managebde | Select-String -Pattern "Used Space Only Encrypted"
 
-#
+$bitlockerfolder = "C:\Bitlocker"
+if (!(Test-Path $bitlockerfolder)) {
+    New-Item -Path $bitlockerfolder -ItemType Directory
+}
+
 # Define exit codes
-#
 $Error_Success = 0
 $Error_OsLegacy = 10
 $Error_Os32Bit = 11
@@ -53,9 +57,7 @@ $Error_BLEncryptionInProgress = 31
 $Error_BLDecryptionInProgress = 32
 $Error_BLFullyEncrypted = 32
 
-#
 # Check if boot mode is UEFI or LEGACY
-#
 if ($FirmwareType -ne "UEFI") {
     Write-Host "OS Boot Mode is LEGACY."
     Write-Host "TPM is not supported with this Boot Mode."
@@ -68,9 +70,7 @@ else {
     Write-Log -Message "OS boot mode is UEFI" -Severity Info
 }
 
-#
 # Check if architecture is 32 Bit or 64 Bit
-#
 if ($OSArchitecture -ne "64-bit") {
     Write-Host "OS architecture is 32 bit"
     Write-Host "TPM is not supported with this OS architecture"
@@ -83,9 +83,7 @@ else {
     Write-Log -Message "OS architecture is 64 bit" -Severity Info
 }
 
-#
 # Check if OS release id is supported (below 1809 is not supported)
-#
 if ($OSReleaseId -lt '1809') {
     Write-Host "OS ReleaseId is lower than 1809 and will not be supported"
     Write-Log -Message "OS ReleaseId is lower than 1809 and will not be supported" -Severity Error
@@ -96,9 +94,7 @@ else {
     Write-Log -Message "OS ReleaseId is supported." -Severity Info
 }
 
-#
 # Check if TPMPresent is true
-#
 if (!$TPMPresent) {
     Write-Host "TPM is not present. Please check the BIOS settings (Trusted Platform)"
     Write-Log -Message "TPM is not present. Please check the BIOS settings (Trusted Platform)" -Severity Error
@@ -109,9 +105,7 @@ else {
     Write-Log -Message "TPM is present" -Severity Info
 }
 
-#
 # Check if TPM is ready
-#
 if (!$TPMReady) {
     Write-Host "TPM is not ready"
     Write-Log -Message "TPM is not ready" -Severity Error
@@ -122,9 +116,7 @@ else {
     Write-Log -Message "TPM is ready" -Severity Info
 }
 
-#
 # Check if MountPoint exists
-#
 if (!$BitLockerReadyDrive) {
     Write-Host "Bitlocker MountPoint does not exist"
     Write-Log -Message "Bitlocker MountPoint does not exist" -Severity Error
@@ -135,37 +127,45 @@ else {
     Write-Log -Message "Bitlocker MountPoint exists" -Severity Info
 }
 
+
 function InitializeTpm {
-    #
     # Initialize TPM, add recovery partition if needed and enable Bitlocker
-    #
     Initialize-Tpm -AllowClear -AllowPhysicalPresence
     BdeHdCfg -target $env:SystemDrive shrink -quiet
     Enable-BitLocker -MountPoint $env:SystemDrive -RecoveryPasswordProtector -SkipHardwareTest
     Write-Log -Message "TPM initialized, recovery partition added if needed and Bitlocker enabled." -Severity Info
 
+    while ((Get-BitLockerVolume -MountPoint $env:SystemDrive -ErrorAction SilentlyContinue).VolumeStatus -eq "EncryptionInProgress") {
+        Write-Host "Encryption is in progress..."
+        Write-Host "Drive $drive is encrypting. Checking again in 60 seconds."
+        Start-Sleep -Seconds 60
+    }
+
+    if ((Get-BitLockerVolume -MountPoint $env:SystemDrive -ErrorAction SilentlyContinue).VolumeStatus -eq "EncryptionInProgress") {
+        Write-Host "Encryption is done"
+        Write-Log -Message "Encryption is done" -Severity Warning
+    }
+    else {
+        Write-Host "Encryption not working or another error."
+        Write-Log -Message "Encryption not working or another error." -Severity Info
+    }
 }
 
-#
 # Add Bitlocker Protectors
-#
 function AddBitlockerProtectors {
-    #Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -TpmProtector -ErrorAction SilentlyContinue
-    #Write-Log -Message "Adding Bitlocker TpmProtector" -Severity Info
-    Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector -ErrorAction SilentlyContinue
-    Write-Log -Message "Adding Bitlocker RecoveryPasswordProtector" -Severity Info
+    # Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -TpmProtector -ErrorAction SilentlyContinue
+    # Write-Log -Message "Adding Bitlocker TpmProtector" -Severity Info
+    # Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector -ErrorAction SilentlyContinue
+    # Write-Log -Message "Adding Bitlocker RecoveryPasswordProtector" -Severity Info
 }
 
-
-#
 # Check Bitlocker status states
-#
 switch ($BitLockerDecrypted) {
     "FullyDecrypted" {
         Write-Host "Bitlocker volume is fully decrypted"
         Write-Log -Message "Bitlocker volume is fully decrypted" -Severity Info
         InitializeTpm
-        AddBitlockerProtectors
+        # AddBitlockerProtectors
     }
     "EncryptionInProgress" {
         Write-Host "Bitlocker volume encryption is in progress. Stopping at this point."
@@ -175,16 +175,39 @@ switch ($BitLockerDecrypted) {
     "DecryptionInProgress" {
         Write-Host "Bitlocker volume decryption is in progress. Stopping at this point"
         Write-Log -Message "Bitlocker volume decryption is in progress. Stopping at this point" -Severity Error
-        Exit $Error_BLDecryptionInProgress
+        try {
+            while ($true) {
+
+                $drive = "C:"
+                # Get BitLocker status
+                $VolumeStatus = Get-BitLockerVolume -MountPoint $drive | Select VolumeStatus
+                
+                if ($VolumeStatus -match "FullyDecrypted") {
+                    Write-Host "Drive $drive is fully Decrypted."
+                    break # Exit loop
+                }
+                else {
+                    Write-Host "Drive $drive is decrypting. Checking again in 60 seconds."
+                    Start-Sleep -Seconds 60 # Wait for 60 seconds before checking again
+                }
+            }
+
+            Write-Host "Encryption check complete."
+        }
+        catch {
+            Write-Host "Error disabling Bitlocker. Stopping at this point."
+            Write-Log -Message "Error disabling Bitlocker. Stopping at this point." -Severity Error
+            Exit $Error_BLDecryptionInProgress
+        }
     }
     "FullyEncrypted" {
-        #check if used space only encryption is enabled
+        # check if used space only encryption is enabled
         if ($null -ne $UsedSpace) {
             Write-Host "Drive already encrypted under used space only. Let's remove this encryption and re-encrypt with full disk encryption."
             Disable-BitLocker -MountPoint $env:SystemDrive
             Start-Sleep -Seconds 120
             InitializeTpm
-            AddBitlockerProtectors
+            # AddBitlockerProtectors
         }
         else {
             Write-Host "Bitlocker volume is already encrypted. Stopping at this point."
@@ -199,10 +222,7 @@ switch ($BitLockerDecrypted) {
     }
 }
 
-
-#
 # Finish
-#
 Write-Host
 Write-Host "Bitlocker has been successfully enabled. Encryption started."
 Write-Log -Message "Bitlocker has been successfully enabled. Encryption started." -Severity Info
---
+++ HPUniversal-DriverScriptOEM.ps1
@@ -0,0 +1,25 @@
+# Define the target driver version
+$targetDriverVersion = "61.270.01.25570"
+
+# Define the path where the INF files are located
+$infPath = "C:\Windows\INF"
+
+# Get all OEM*.inf files in the directory
+$infFiles = Get-ChildItem -Path $infPath -Filter "OEM*.inf"
+
+# Regex pattern to match DriverVer lines
+$pattern = "DriverVer=.*,${targetDriverVersion}"
+
+# Iterate over each file and search for the target driver version
+foreach ($file in $infFiles) {
+    # Read the content of the current INF file
+    $fileContent = Get-Content -Path $file.FullName
+    
+    # Search for the target driver version in the file
+    foreach ($line in $fileContent) {
+        if ($line -match $pattern) {
+            "Driver version $targetDriverVersion found in file: $($file.Name)"
+            break # Stop checking this file and move to the next
+        }
+    }
+}
