238e3a43b666358ff197f1423a273738dee57470
Adam <27519+adamgell@users.noreply.github.com>
2024-07-15 13:27:05 -0400

    intune remediation scripts

---
+++ Intune/WUFB/Remedition/Detect_EnablePreviewBuilds.ps1
@@ -0,0 +1,39 @@
+# Define the registry paths
+$registryPaths = @(
+    'HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Update',
+    'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate',
+    'HKLM:\SOFTWARE\POLICIES\Microsoft\Windows\Update\AU',
+    'HKLM:\SOFTWARE\POLICIES\Microsoft\Windows\Update'
+)
+
+# Define the registry entries to check
+$registryEntries = @("ManagePreviewBuilds", "ManagePreviewBuilds_ProviderSet", "ManagePreviewBuilds_WinningProvider")
+
+# Flag to track if any registry entry is found
+$entryFound = $false
+
+# Loop through each registry path
+foreach ($registryPath in $registryPaths) {
+    # Check if the registry path exists
+    if (Test-Path $registryPath) {
+        # Loop through each registry entry
+        foreach ($entry in $registryEntries) {
+            # Check if the registry value exists
+            $value = Get-ItemProperty -Path $registryPath -Name $entry -ErrorAction SilentlyContinue
+            if ($null -ne $value) {
+                # Set flag to true if any entry is found
+                $entryFound = $true
+                break
+            }
+        }
+    }
+}
+
+# Output the detection result
+if ($entryFound) {
+    Write-Output "Registry entries found"
+    exit 1  # Indicates remediation is needed
+} else {
+    Write-Output "Registry entries not found"
+    exit 0  # Indicates no remediation needed
+}
