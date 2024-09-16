1f66a1b7f63965d5b5c4ba8f3b0bed2f732a525e
Adam <27519+adamgell@users.noreply.github.com>
2024-09-16 14:03:27 -0400

    Update Install.ps1
    added comments

--- Intune/M365Apps/Teams/Install.ps1
+++ Intune/M365Apps/Teams/Install.ps1
@@ -1,55 +1,45 @@
-<# 
-.SYNOPSIS 
-Install New Microsoft Teams App on target devices 
-.DESCRIPTION 
-Below script will install New MS Teams App Offline.
- 
-.NOTES     
-        Name       : New MS Teams App Installation Offline
-        Author     : Jatin Makhija  
-        Version    : 1.0.0  
-        DateCreated: 12-Jan-2024
-        Blog       : https://cloudinfra.net
-         
-.LINK 
-https://cloudinfra.net 
-#>
 # Define paths and filenames
-$msixFile = ".\MSTeams-x64.msix"
-$destinationPath = "C:\windows\temp"
-$bootstrapperPath = ".\teamsbootstrapper.exe"
+$msixFile = ".\MSTeams-x64.msix" # Path to the Microsoft Teams MSIX package file
+$destinationPath = "C:\windows\temp" # Destination directory where the MSIX file will be copied
+$bootstrapperPath = ".\teamsbootstrapper.exe" # Path to the Teams bootstrapper executable
 
 # Copy MSTeams-x64.msix to the destination directory
 Copy-Item -Path $msixFile -Destination $destinationPath -Force
 
 # Check if the copy operation was successful
 if ($?) {
+    # Uninstall any previously installed Microsoft Teams packages for all users
     Get-AppPackage -AllUsers *MSTeams* | Remove-AppPackage -AllUsers -ErrorAction SilentlyContinue
-    # Uninstall teams if perviously installed
+    
+    # If successful, execute the bootstrapper to perform cleanup operations
     Start-Process -FilePath $bootstrapperPath -ArgumentList "-x" -Wait -WindowStyle Hidden
-    # If successful, execute teamsbootstrapper.exe with specified parameters
+
+    # Execute the bootstrapper again to install the MSIX package from the specified destination path
     Start-Process -FilePath $bootstrapperPath -ArgumentList "-p", "-o", "$destinationPath\MSTeams-x64.msix" -Wait -WindowStyle Hidden
+
+    # Copy the Teams icon to the ProgramData directory for use in shortcuts
     Copy-Item -Path ".\teams.ico" -Destination "C:\ProgramData\Microsoft\teams.ico"
     
     # Create a shortcut on the public desktop
     $wshShell = New-Object -ComObject WScript.Shell
     $shortcut = $wshShell.CreateShortcut("C:\Users\Public\Desktop\Microsoft Teams.lnk")
-    $shortcut.TargetPath = "EXPLORER.EXE"
-    $shortcut.Arguments = "shell:AppsFolder\MSTeams_8wekyb3d8bbwe!MSTeams"
-    $shortcut.IconLocation = "C:\ProgramData\Microsoft\teams.ico"
+    $shortcut.TargetPath = "EXPLORER.EXE" # Set the shortcut target to open Explorer
+    $shortcut.Arguments = "shell:AppsFolder\MSTeams_8wekyb3d8bbwe!MSTeams" # Arguments to launch the Teams app
+    $shortcut.IconLocation = "C:\ProgramData\Microsoft\teams.ico" # Set the Teams icon for the shortcut
     $shortcut.Save()
 
-    #copy to start menu
+    # Copy the shortcut to the Start Menu for all users
     $shortcut = $wshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Teams.lnk")
-    $shortcut.TargetPath = "EXPLORER.EXE"
-    $shortcut.Arguments = "shell:AppsFolder\MSTeams_8wekyb3d8bbwe!MSTeams"
-    $shortcut.IconLocation = "C:\ProgramData\Microsoft\teams.ico"
+    $shortcut.TargetPath = "EXPLORER.EXE" # Set the shortcut target to open Explorer
+    $shortcut.Arguments = "shell:AppsFolder\MSTeams_8wekyb3d8bbwe!MSTeams" # Arguments to launch the Teams app
+    $shortcut.IconLocation = "C:\ProgramData\Microsoft\teams.ico" # Set the Teams icon for the shortcut
     $shortcut.Save()
 
+    # Indicate successful installation
     Write-Host "Microsoft Teams installation completed successfully."
 }
 else {
-    # If copy operation failed, display an error message
+    # If the copy operation failed, display an error message and exit with error code
     Write-Host "Error: Failed to copy MSTeams-x64.msix to $destinationPath."
     exit 1
 }
